c5ad13e5aefb4dfca5c5581000a9a0ce9088e520
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2018-04-20 22:36:33 -0300

    Strings Part II

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -8,7 +8,6 @@ public class IdwallFormatter extends StringFormatter {
 	private Integer limit;
 	private boolean justify;
 	private int start = 0;
-	private int gap;
 
 	public IdwallFormatter(Integer limit, Boolean justify) {
 		this.limit = limit;
@@ -25,43 +24,52 @@ public class IdwallFormatter extends StringFormatter {
     public String format(String text) {
     	StringBuilder newText = new StringBuilder();
     	while(text.length() > start) {
-    		gap = 0;
     		if(text.length() < (start + limit)) {
     			newText.append(lastLine(text, start));
     		} else {
-    			newText.append(makingLine(text, start, start + limit) + "\n");
+    			newText.append(makeLine(text, start, start + limit) + "\n");
     		}
     	}
     	return newText.toString();
     }
     
-	private String makingLine(String text, int startLine, int endLine) {
+	private String makeLine(String text, int startLine, int endLine) {
 		if(Character.toString(text.charAt(startLine)).matches("[\\S]")) {
 			if(text.substring(endLine - 1, endLine).matches("[\\s]") || text.substring(endLine, endLine + 1).matches("[\\s]")) {
 				start = endLine;
-				return justify && gap != 0 ? justifyLine(text.substring(startLine, endLine), gap) : text.substring(startLine, endLine);
+				return justify ? justifyLine(text.substring(startLine, endLine)) : text.substring(startLine, endLine);
 			} else {
-				gap++;
-				return makingLine(text, startLine, endLine - 1);
+				return makeLine(text, startLine, endLine - 1);
 			}
 		} else {
-			return makingLine(text, startLine + 1, endLine + 1);
+			return makeLine(text, startLine + 1, endLine + 1);
 		}
 	}
 	
 	private String lastLine(String text, int startLine) {
 		if(Character.toString(text.charAt(startLine)).matches("[\\S]")) {
 			start = text.length();
-			return text.substring(startLine);
+			return justify ? justifyLine(text.substring(startLine)) : text.substring(startLine);
 		} else {
 			return lastLine(text, startLine + 1);
 		}
 	}
 	
-	private String justifyLine(String line, int gap) {
-		/**
-		 * Magic
-		 */
+	private String justifyLine(String line) {
+		if(line.endsWith(" ")) {
+			line = line.substring(0, line.length() - 1);
+		}
+		int i = line.length();
+		while(line.length() < limit) {
+			if(line.substring(i - 1, i).matches("[\\s]")) {
+				line = line.substring(0, i).concat(" ").concat(line.substring(i));				
+			}
+			if(i > 1) {
+				i--;
+			} else {
+				i = line.length();
+			}
+		}
 		return line;
 	}
 }
